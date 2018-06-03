75625b7229a5d85ae664f680b2c1b333eece59a3
Tega Adigu <tega.adigu@gmail.com>
2018-06-03 15:58:11 -0600

    added terminal flow for app, and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # RC4-PHP
 Simple implementation of RC4 Encryption / Decryption in php with test cases
+
+### Installation
+
+1) git clone this module ``` git clone git@github.com:tegaadigu/RC4-PHP.git ```
+2) ``` cd ``` into project directory
+3) Run ``` composer install ``` to install all dependencies ( mainly phpunit )
+
+Congratulations you've installed the project.
+
+### Running The App
+In your terminal, enter ``` php app.php ``` (this assumes you have the php cli installed)
+if not download [php_cli](http://php.net/downloads.php)
+
+### Running Test Cases
+
+This application uses phpunit for its test cases. To run the tests, enter ``` phpunit tests ```
+from the root directory of the project.
+
+ps you can download [phpunit](https://phpunit.de/) if you dont already have it installed also install it globally.
--- app.php
+++ app.php
@@ -1,1 +1,53 @@
 <?php
+
+include __DIR__ . '/vendor/autoload.php';
+
+use App\RC4;
+
+$rc4 = new RC4();
+
+function enterCipherKey(RC4 $rc4, bool $decrypt = false) : void {
+  echo "Enter cipher key (make sure key length > 0 < 255)\n key:";
+  $rc4->setKey(readStream());
+  echo 'Enter string you\'d like to ', ($decrypt ? 'decrypt' : 'encrypt'), PHP_EOL, 'string:';
+  $result = $decrypt ? $rc4->decrypt(readStream(), true) : $rc4->encrypt(readStream(), true);
+  echo 'Your ', ($decrypt ? 'decrypted' : 'encrypted'), 'string is : ', $result.PHP_EOL;
+}
+
+/**
+ * @return string
+ */
+function readStream() : string {
+  $handle = fopen ('php://stdin', 'rb');
+  return trim(fgets($handle));
+}
+
+/**
+ *
+ */
+function endProgram() : void {
+  echo 'Thank you for trying out RC4 Program:' . PHP_EOL;
+  exit;
+}
+
+$line = '';
+do{
+  echo "Welcome to RC4 Cipher program\n\nChoose From the below Option\n\n 1: Encrypt \n 2: Decrypt \n 3: exit/quit \n\n option:";
+  $line = readStream();
+  if(in_array($line, ['exit', '3', 'quit'], true)){
+    endProgram();
+  }
+  switch ($line) {
+    case '1':
+      enterCipherKey($rc4);
+      break;
+    case '2':
+      enterCipherKey($rc4, true);
+      break;
+    default:
+      exit();
+  }
+}while($line = 'yes');
+echo "\n";
+echo "Thank you, continuing...\n";
+
